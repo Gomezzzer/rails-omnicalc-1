a4943983500fdca8b807b6141e56f81e6d0e8a72
Jorge Gomez <86806874+Gomezzzer@users.noreply.github.com>
2024-02-08 04:54:36 +0000

    Dropped parentheses

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
-  get("/", controller: "calc", action: "square")
-  get("/square/new", controller: "calc", action: "square")
+  get "/", controller: "calc", action: "square" 
+  get "/square/new", controller: "calc", action: "square"
 
-  get("/square/results", controller: "calc", action: "square_result")
+  get "/square/results", controller: "calc", action: "square_result"
 
   # Square root calc routes
-  get("/square_root/new", controller: "calc", action: "root")
-  get("/square_root/results", controller: "calc", action: "root_result")
+  get "/square_root/new", controller: "calc", action: "root"
+  get "/square_root/results", controller: "calc", action: "root_result"
 
   # Payment routes
-  get("/payment/new", controller: "calc", action: "payment")
-  get("/payment/results", controller: "calc", action: "payment_result")
+  get "/payment/new", controller: "calc", action: "payment"
+  get "/payment/results", controller: "calc", action: "payment_result"
 
   # random routes
-  get("/random/new", controller: "calc", action: "random")
-  get("/random/results", controller: "calc", action: "random_result")
+  get "/random/new", controller: "calc", action: "random"
+  get "/random/results", controller: "calc", action: "random_result"
 end
